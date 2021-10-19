57109ed09109afc9f8c669610adfd9be1f6e75ba
vitorSinqia <vitor.andrade@sinqia.com.br>
2021-10-19 19:25:33 -0300

    Inicio do projeto de Jogo RPG

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using CursoPOO_DIO.src.entities;
+namespace CursoPOO_DIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Knight arus = new Knight("Arus", 23, "Knight ");
+            Wizard wizard = new Wizard("Jenica", 23 , "White Wizard");
+            
+            Console.WriteLine(arus.Attack());
+            Console.WriteLine(wizard.Attack(30));
+        }
+    }
+}
---
+++ src/entities/Knight.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoPOO_DIO.src.entities
+{
+    class Knight : Hero
+    {
+        public Knight(string Name, int Level, string HeroType) : base (Name,Level, HeroType)
+        {
+            this.Name = Name;
+            this.Level = Level;
+            this.HeroType = HeroType;
+        }
+        
+        public override string Attack()
+        {
+            return this.Name + "Atacou";
+        }
+    }
+}
